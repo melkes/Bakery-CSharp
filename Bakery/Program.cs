904779d9a595d344929e5056efc167416ef829d6
Mike Wilkes <mikewilkes@outlook.com>
2023-02-12 04:35:42 -0800

    Program.cs is now working correctly

--- Bakery/Program.cs
+++ Bakery/Program.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using Bakery;
 
 namespace Bakery
 {
@@ -10,6 +11,24 @@ namespace Bakery
       Console.WriteLine("Welcome to Pierre's Bakery! Here are our prices:");
       Console.WriteLine("Bread: $5/loaf (buy 2 get 1 free)");
       Console.WriteLine("Pastries: $2/each (buy 3 get 1 free)");
+      Console.WriteLine("How many loaves of bread would you like?");
+      int inputBread = int.Parse(Console.ReadLine());
+      Console.WriteLine("And how many pastries would you like?");
+      int inputPastry = int.Parse(Console.ReadLine());
+
+      if ((inputBread + inputPastry) >= 1)
+      {
+        Bread bread = new Bread();
+        Pastry pastry = new Pastry();
+
+        int totalPrice = bread.CalculatePrice(inputBread) + pastry.CalculatePrice(inputPastry);
+        Console.Write("The total price of your order is: $" + totalPrice);
+      }
+      else
+      {
+        Console.WriteLine("Something went wrong, or you entered 0 for both bread and pastry orders. Try again!");
+        Main();
+      }
 
     }
   }
