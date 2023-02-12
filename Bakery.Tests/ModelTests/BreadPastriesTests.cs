02a217b50c8fd83b7f000874b5ec89c1483eaccb
Mike Wilkes <mikewilkes@outlook.com>
2023-02-12 03:12:01 -0800

    added test for calculating combined bread+pastry order price

--- Bakery.Tests/ModelTests/BreadPastriesTests.cs
+++ Bakery.Tests/ModelTests/BreadPastriesTests.cs
@@ -42,6 +42,16 @@ namespace Bakery.Tests
       int result = pastry.CalculatePrice(numberOfPastries);
       Assert.AreEqual(expectedResult, result);
     }
-
+    [TestMethod]
+    public void CalculatePricePastryAndBread_ValidInput_ReturnsPrice()
+    {
+      int numberOfPastries = 6;
+      int numberOfLoaves = 3;
+      Pastry pastry = new Pastry();
+      Bread bread = new Bread();
+      int expectedResult = 20;
+      int result = pastry.CalculatePrice(numberOfPastries) + bread.CalculatePrice(numberOfLoaves);
+      Assert.AreEqual(expectedResult, result);
+    }
   }
 }
